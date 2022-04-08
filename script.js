e79b6cbfe387d622733050e5ef9f353dd3c92dd0
krishna8223 <krishnaverma.kv822@gmail.com>
2022-04-08 12:04:16 +0530

    added top scroll bar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const cross = document.getElementsByClassName('nav__cross')[0]
 const hamburger = document.getElementsByClassName('nav__hamburger')[0]
 const menu = document.getElementsByClassName('nav__menu')[0]
 const hero = document.getElementsByClassName('hero')[0]
+const topScroll = document.getElementsByClassName('top-scroll')[0]
 
 
 cross.addEventListener('click',nav)
@@ -13,3 +14,11 @@ function nav() {
     hamburger.classList.toggle('hide')
     cross.classList.toggle('hide')
 }
+
+document.addEventListener('scroll',(e)=>{
+
+    const  scrolled = window.scrollY / (document.body.scrollHeight - window.innerHeight) * 100
+    const width = Math.round(scrolled)  
+    topScroll.style.width = width+'%'
+
+})
